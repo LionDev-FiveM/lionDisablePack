7dc7fa4bca454424615830e59902e73545826101
Lion Development <157517442+LionDev-FiveM@users.noreply.github.com>
2024-01-24 21:49:59 +0100

    Add files via upload

--- server.lua
+++ server.lua
@@ -43,16 +43,11 @@ end
 
 
 
-
-
-
-
-
 Citizen.CreateThread(function()
     local vRaw = LoadResourceFile(GetCurrentResourceName(), 'version.json')
     if vRaw and Config.VersionCheck then
         local v = json.decode(vRaw)
-        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionDRP/main/lionDRP/version.json'
+        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionDisablePack/main/version.json'
         PerformHttpRequest(url, function(code, res)
             if code == 200 then
                 local rv = json.decode(res)
